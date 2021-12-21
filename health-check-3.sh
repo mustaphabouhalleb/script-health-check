2a28e26003077792a2883ac93a7dfa02c2d75714
mustaphabouhalleb <70152711+mustaphabouhalleb@users.noreply.github.com>
2021-12-21 19:42:36 +0100

    Update health-check-3.sh

--- health-check-3.sh
+++ health-check-3.sh
@@ -1,4 +1,13 @@
 #!/bin/bash 
+##-----NOTE: This script requires root privileges, otherwise anyone could run the script -------##
+##---- as a sudo user who got root privileges. -------------------------------------------------##
+##----------- "sudo /bin/bash <ScriptName>" ----------------------------------------------------##
+
+AUTHOR="Mustapha Bouhalleb"
+CONTACT="mbouhall@redhat.com"
+DATE="22.12.2021"
+VERSION="1.0.2"
+
 function sysstat {
 echo -e "
 #####################################################################
