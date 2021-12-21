eb9efbd243a4f64fa9daaca9ef8b689eb935cefd
mustaphabouhalleb <70152711+mustaphabouhalleb@users.noreply.github.com>
2021-12-21 19:16:09 +0100

    Update health-check-6.sh

--- health-check-6.sh
+++ health-check-6.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-#########################################
-## name:            Linux healthcheck  ##
-## author:          jjapachehe         ##
-#########################################
 
 #Global variables
 date=$(date +"%Y%m%d")
@@ -62,10 +58,10 @@ else
     printf "Zombie process status:\t No zombie process\n"
 fi
 separator
-banner "NTP and synchronization"
-printf "NTP information:\t"; ntpstat | awk 'NR==1 {print $0}'
-printf "NTP lead field:\t\t"; ntpq -c rv | awk 'NR==1 {print $3}'
-printf "NTP reach value:\t"; ntpq -p | awk 'NR==4 {print $7}'
+#banner "NTP and synchronization"
+#printf "NTP information:\t"; ntpstat | awk 'NR==1 {print $0}'
+#printf "NTP lead field:\t\t"; ntpq -c rv | awk 'NR==1 {print $3}'
+#printf "NTP reach value:\t"; ntpq -p | awk 'NR==4 {print $7}'
 separator
 printf "\t\t Time and Date status\n"
 timedatectl
@@ -82,10 +78,10 @@ printf "\t\tNetwork interface statistics\n"
 netstat -i | grep -v ^lo | column -t
 separator
 printf "\t\tCurrent bandwidth usage\n"
-for interface in $(ip link show | awk '{print $2}' | grep -v '^[0-9]' | grep -v "@"| sed 's/:$//')
-do
-    printf "${interface}: "; sar 1 1 -n DEV | grep ${interface} | grep -v ^Average | tail -1 | awk '{print $6+$7 " Mb"}'
-done
+#for interface in $(ip link show | awk '{print $2}' | grep -v '^[0-9]' | grep -v "@"| sed 's/:$//')
+#do
+#    printf "${interface}: "; sar 1 1 -n DEV | grep ${interface} | grep -v ^Average | tail -1 | awk '{print $6+$7 " Mb"}'
+#done
 separator
 printf "\t\t static routes\n"
 route -n
@@ -104,12 +100,12 @@ separator
 
 printf "\n\n\t\t HealthChek Summary Report\n\n"
 printf "Time UP:\t\t"; uptime|sed 's/.*up \([^,]*\), .*/\1/' | awk '{if ($1 > 0) print "HEALTHY"; else print "WARNING"}'
-printf "CPU Utilization:\t"; mpstat -P ALL 1 5 -u | grep "^Average" | sed "s/Average://g" | grep -w "all" | awk '{print $NF}' | awk -F'.' '{print(100 -$1)}' | awk '{if($1 < 70) print "HEALTHY"; else print "WARNING"}'
+#printf "CPU Utilization:\t"; mpstat -P ALL 1 5 -u | grep "^Average" | sed "s/Average://g" | grep -w "all" | awk '{print $NF}' | awk -F'.' '{print(100 -$1)}' | awk '{if($1 < 70) print "HEALTHY"; else print "WARNING"}'
 printf "Memory Utilization:\t"; vmstat -s | grep -w "used memory" | awk '{printf(" %.0f", $1/1024/1024)}' | awk '{if($1 < 700) print "HEALTHY"; else print "WARNING"}'
 printf "SWAP Usage:\t\t"; vmstat -s | grep -w "used swap" | awk '{printf(" %.0f", $1/1024/1024)}' | awk '{if($1 < 20) print "HEALTHY"; else print "WARNING" }'
 printf "Load Average:\t\t"; uptime|grep -o "load average.*"|awk '{print  $3}' | sed 's/,$//' | awk '{if($1 <= 15) print "HEALTHY"; else print "WARNING" }'
 printf "Zombie Process:\t\t"; if [ $num_zomb_proc -gt 0 ]; then printf "WARNING\n"; else printf "HEALTHY\n"; fi
-printf "NTP Sincronization:\t"; ntpq -p | awk 'NR==4 {print $7}' | awk '{if($1 == 377) print "HEALTHY"; else print "WARNING"}'
+#printf "NTP Sincronization:\t"; ntpq -p | awk 'NR==4 {print $7}' | awk '{if($1 == 377) print "HEALTHY"; else print "WARNING"}'
 printf "Network Errors:\t\t"; netstat -i|egrep -v "Iface|statistics"|awk '{sum += $4;sum += $8} END {print sum}' | awk '{if($1 == 0) print "HEALTHY"; else print "WARNING"}'
 printf "Disk Space Usage:\t"; df -Ph|egrep -v "^Filesystem|mnt|tmp" | awk '{print $5,$6}' |sort -n |tail -1 | awk '{if($1 <=80) print "HEALTHY"; else print "WARNING"}'
 printf "Message Errors:\t\t"; sudo tail -50 /var/log/messages|egrep -i "warning|error" | wc -l | awk '{if($1 == 0) print "HEALTHY"; else print "WARNING"}'
